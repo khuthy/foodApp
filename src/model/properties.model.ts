618b71a6f24dffca75afc8061744229a3ace03cb
Muenda Khuthadzo <muendakhuthadzo@gmail.com>
2019-07-01 08:20:28 -0700

    uploading an imitated design

---
+++ src/model/properties.model.ts
@@ -0,0 +1,12 @@
+export class Properties { 
+  
+     public   image: string;
+     public   title: string;
+     public   subtitle: string;
+     public   rate: string;
+     public   cookingTime: string
+     public   purchase: string;
+     public   heart: boolean;
+   
+     
+ }
---
+++ src/pages/home/home.ts
@@ -0,0 +1,40 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
+import { Food } from '../../model/foodSlider/food.model';
+
+
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+})
+export class HomePage {
+  @ViewChild(Slides) slides: Slides;
+  
+  foods: any[];
+  options: any;
+  sliderConfig: any = {};
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.foods = Food;
+     
+      
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+    console.log(this.foods);
+  }
+  searchAddress(q){
+    console.log('searched!!!!');
+    
+  }
+
+}
